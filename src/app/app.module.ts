11604a185aba15a5241c96c0fb9aa42b852ce615
williamevann1805 <c14180222@john.petra.ac.id>
2020-04-22 10:27:21 +0700

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ListComponent } from './list/list.component';
+import { InputComponent } from './input/input.component';
+import { EditComponent } from './edit/edit.component';
+import{Routes, RouterModule } from '@angular/router';
+
+const ROUTES:Routes = [
+  { path:'input', component : InputComponent },
+  { path:'edit', component : EditComponent },
+  { path:'list', component : ListComponent }
+]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+  BrowserModule, 
+  FormsModule,
+  RouterModule.forRoot(ROUTES)
+  ],
+  declarations: [ AppComponent, HelloComponent, InputComponent, EditComponent, ListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
