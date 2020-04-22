7d092ec678cee7f8050d69604fb3e1a171b4c415
williamevann1805 <c14180222@john.petra.ac.id>
2020-04-22 10:37:27 +0700

    2 files modified, 1 added

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : ActivatedRoute) { }
+
+  namaTemp = '';
+  penjelasanTemp = '';
+  arrNama:String[];
+  arrPenjelasan:String[];
 
   ngOnInit() {
+    let nama = this.route.snapshot.paramMap.get('txtNama');
+    this.namaTemp = nama;
+    let penjelasan = this.route.snapshot.paramMap.get('txtPenjelasan');
+    this.penjelasanTemp = penjelasan;
   }
 
+  show()
+  {
+    this.arrNama.push(this.namaTemp);
+    this.arrPenjelasan.push(this.penjelasanTemp);
+  }
 }
